46847751f86d5a832c6dcc454ea177616c731615
asanmun1905 <adri1952005.4@gmail.com>
2026-09-24 12:45:21 +0200

    started cv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cv';
+  nombre = 'Adrián Sánchez Muñoz'
+  fecha = new Date();
+  grado = 'Desarrollo de Aplicaciones Multiplataforma';
+  titulo = 'Mi CV en Angular';
+  ciudad = 'Málaga';
+  telefono = '+34 697 292 503';
+  email = 'adri1952005.4@gmail.com';
+  github = 'https://github.com/asanmun1905';
+  idiomas = 'Español, Inglés';
 }
